7ec3ef4bf72eb1ab90e9c29f1569b457c5faf311
Biri1995 <ranndo.parish966@slmails.com>
2024-10-01 20:40:38 +0200

    Update

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ read -p "Start setup? (y/n) " yn
 
 	echo executing...
 
+#vscodium repo 
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
@@ -19,15 +20,25 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 
+#update & package install
 sudo apt update
 
-sudo apt install i3 feh firefox-esr dmenu xorg neovim sudo kitty neofetch ranger git picom codium -y
+sudo apt install i3 feh firefox-esr dmenu xorg neovim sudo kitty neofetch ranger git picom codium lxappearance gtk-chtheme -y
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip
+#install fonts
+read -p "Install fonts? (y/n) " yn
+	case $yn in
+		y ) echo proceeding...;
+			wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip;
+			wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/BigBlueTerminal.zip;
+			sudo unzip BigBlueTerminal.zip -d /usr/local/share/fonts;
+			sudo unzip 0xProto.zip -d /usr/local/share/fonts;;
+		n ) echo skipping...;;
+		* ) echo invalid input;
+			exit 1;;
+	esac
 
-sleep 5
-
-sudo unzip 0xProto.zip -d /usr/local/share/fonts
+	echo executing...
 
 #i3 window manager setup
 read -p "Setup i3 window manager? (y/n) " yn
@@ -37,7 +48,9 @@ read -p "Setup i3 window manager? (y/n) " yn
 			mkdir $HOME/.config/i3;
 			cp config/i3/config $HOME/.config/i3;
 			mkdir $HOME/.config/picom;
-			cp config/picom/picom.conf $HOME/.config/picom;;
+			cp config/picom/picom.conf $HOME/.config/picom;
+			mkdir $HOME/.config/polybar/polybar.conf;
+			cp config/polybar/config.ini config/polybar/launch.sh $HOME/.config/polybar;;
 
 		n ) echo skipping...;;
 		* ) echo invalid input;
@@ -58,4 +71,16 @@ read -p "Setup kitty console? (y/n) " yn
 			exit 1;;
 	esac
 
-
+#ohmyzsh setup
+read -p "Setup ohmyzsh? (y/n) " yn
+	case $yn in
+		y ) echo proceeding...;
+			sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)";
+			git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
+			rm ~/.zshrc;
+			cp oh-my-zsh/zshrc $HOME/.zshrc;
+			echo Restart terminal to finish setup.;;
+		n ) echo skipping...;;
+		* ) echo invalid input;
+			exit 1;;
+	esac
